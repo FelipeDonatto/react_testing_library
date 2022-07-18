8b03d4191e9ae1950b729a4d1fbc67c1fc769372
Felipe <fe.donatto@outlook.com>
2022-07-18 16:50:01 -0300

    4 ✔️

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent, screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { NotFound } from '../pages';
+
+describe('Testa o componente NotFound', () => {
+  it('', () => {
+    renderWithRouter(<NotFound />);
+    expect(
+      screen.getByRole('heading', { name: /page requested not found/i }),
+    ).toBeDefined();
+    const imgSource = screen
+      .getByRole('img',
+        { name: /pikachu crying because the page requested was not found/i });
+    const { src } = imgSource;
+    expect(src).toBe('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
