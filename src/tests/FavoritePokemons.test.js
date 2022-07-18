dcd09a9c18d8ec1c77692f54ecb525f2e56f8fda
Felipe <fe.donatto@outlook.com>
2022-07-18 16:31:07 -0300

    req 3 ✔️

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { fireEvent, screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Testa se o componente favorite', () => {
+  test('mostra no favorite pokemon', () => {
+    renderWithRouter(<App />);
+    fireEvent.click(screen.getByRole('link', { name: /favorite pokémons/i }));
+    expect(screen.getByText(/no favorite pokemon found/i)).toBeInTheDocument();
+  });
+  test('mostra os pokemon favoritos', () => {
+    renderWithRouter(<App />);
+    fireEvent.click(screen.getByRole('link', { name: /more details/i }));
+    fireEvent.click(screen.getByRole('checkbox', { name: /pokémon favoritado\?/i }));
+    fireEvent.click(screen.getByRole('link', { name: /favorite pokémons/i }));
+    expect(screen.getByText(/pikachu/i)).toBeInTheDocument();
+    // expect(screen.getByText(/no favorite pokemon found/i)).not.toBeInTheDocument();
+  });
+});
